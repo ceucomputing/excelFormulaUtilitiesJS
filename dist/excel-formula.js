2ca7294f3dd52513fa9342b1b6f0a57864773b02
jiunwei <jiunwei@gmail.com>
2018-10-23 16:13:48 +0800

    Collapse tokens of identical type

--- dist/excel-formula.js
+++ dist/excel-formula.js
@@ -748,7 +748,12 @@
 
         while (tokens2.moveNext()) {
             if (tokens2.current().type.toString() !== TOK_TYPE_NOOP) {
-                tokens.addRef(tokens2.current());
+                var token = tokens2.current();
+                while (tokens2.next() && tokens2.next().type === token.type && tokens2.next().subtype === token.subtype) {
+                    tokens2.moveNext();
+                    token.value += tokens2.current().value;
+                }
+                tokens.addRef(token);
             }
         }
 
